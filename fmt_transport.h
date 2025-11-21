14159a52f428df13a3a91a0bbfc5c7b02cf843e0
mthielvoldt <mthielvoldt@gmail.com>
2025-11-21 11:53:01 -0500

    Remove dependency on queue

--- fmt_transport.h
+++ fmt_transport.h
@@ -1,20 +1,19 @@
 /** fmt_transport.h
  * Common interface for the firment transport layers (spi, uart, etc.)
- * Dependency graph: (interfaces)
- *
- * fmt_base.c -> (fmt_transport.h) -> [fmt_transport_spi.c / fmt_transport_uart.c]
+ 
+ * Dependency graph:
+ * firment-base -> fmt_transport.h <-implements- firment-transport_xxx
  *
  * These are function pointers so:
  * 1. Several communication transports (spi, uart, can...) may implement this interface.
  * 2. Unit tests can cover fmt_comms.c using spys at the transport layer.
  * 3. Unit tests can compile all transports and switch which one is active.
  */
-#ifndef fmt_transport_H
-#define fmt_transport_H
+#ifndef FMT_TRANSPORT_H
+#define FMT_TRANSPORT_H
 
 #include <stdint.h>
 #include <stdbool.h>
-#include "queue.h"
 
 typedef volatile struct _transportErrCount
 {
@@ -24,12 +23,19 @@ typedef volatile struct _transportErrCount
   uint8_t modeFault;
 } transportErrCount_t;
 
-typedef void (*rxCallback_t)(const uint8_t *rxData);
+typedef void (*rxCallback_t)(const uint8_t * const rxData);
+typedef bool (*txCallback_t)(uint8_t * const txBuffer);
 
-extern void (*fmt_startTxChain)(void);
-extern bool (*fmt_linkTransport)(queue_t *sendQueue, rxCallback_t rxCallback);
-extern const transportErrCount_t *(*fmt_getTransportErrCount)(void);
+// Define callback types.
+typedef bool (*fmt_linkTransport_t)(txCallback_t pullTxPacket, rxCallback_t pushRxPacket);
+typedef void (*fmt_startTxChain_t)(void);
+typedef const transportErrCount_t *(*fmt_getTransportErrCount_t)(void);
+
+// Declare function pointers.  Transport implementation will define these.
+extern fmt_startTxChain_t fmt_startTxChain;
+extern fmt_linkTransport_t fmt_linkTransport;
+extern fmt_getTransportErrCount_t fmt_getTransportErrCount;
 
 bool fmt_initTransport(void);
 
-#endif // fmt_transport_H
+#endif // FMT_TRANSPORT_H
